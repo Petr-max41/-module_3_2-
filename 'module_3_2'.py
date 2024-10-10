e44e975dcb364cee30b68ec09704c267145f6f51
Petr-max41 <krilof25@mail.ru>
2024-10-10 13:10:15 +0500

    Add files via upload

---
+++ 'module_3_2'.py
@@ -0,0 +1,20 @@
+def send_email(message, recipient, sender = "university.help@gmail.com"):
+    if ("@" and (".com" or ".ru" or ".net") ) not in (recipient or sender) or ("@" or (".com" or ".ru" or ".net"))\
+        not in (recipient or sender):
+        print(f"'Невозможно отправить письмо с адреса' {sender} 'на адрес' {recipient}")
+    elif sender == recipient:
+        print("'Нельзя отправить письмо самому себе!'")
+    elif sender == "university.help@gmail.com":
+        print(f"'Письмо успешно отправлено с адреса' {sender} 'на адрес' {recipient}")
+    elif sender != "university.help@gmail.com":
+        print(f"'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса' {sender} 'на адрес' {recipient}")
+
+
+
+
+send_email('message', "university.help@gmail.com", '@mail.com' )
+send_email('message', "university.help@gmail.com" )
+send_email('message', '@mail.com' )
+send_email('message', '@mail.om')
+send_email('message', 'mail.om')
+
